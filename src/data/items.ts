693c62788423d482a3aba6df65920bb319a4f410
Pierri Alexander Vidmar <pierri@hotmail.com.br>
2024-03-30 14:30:25 -0300

    Gerado dados mocados e tipagem

---
+++ src/data/items.ts
@@ -0,0 +1,28 @@
+import { Item } from '../types/Item';
+
+export const items: Item[] = [
+  { 
+    date: new Date(2021, 9, 15),
+    category: 'food',
+    title: 'McDonalds',
+    value: 32.12
+  },
+  { 
+    date: new Date(2021, 9, 15),
+    category: 'food',
+    title: 'Burger King',
+    value: 28
+  },
+  { 
+    date: new Date(2021, 9, 16),
+    category: 'rant',
+    title: 'Aluguel Apt',
+    value: 1200
+  },
+  { 
+    date: new Date(2021, 10, 18),
+    category: 'salary',
+    title: 'Salário do Mês',
+    value: 4500
+  },
+]
